d119fd32216fa9984c06be884c3fe1b1ac943b96
harsh71019 <harsh710patel@gmail.com>
2021-02-03 00:04:49 +0530

    trabsnfer

---
+++ pages/portfolios/[id].js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useRouter } from "next/router";
+const PorfolioDetail = () => {
+//   const router = useRouter();
+//   const { id } = router.query;
+return <div>Hey i am detail page ID : {id}</div>;
+};
+
+export default PorfolioDetail;
+
